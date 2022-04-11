51c38877f7f0b4457d909f2b5538aaf1b4207753
Mateus Braga <mateuscoclans@gmail.com>
2022-04-11 19:07:37 -0300

    Update expressoes_logicas.cpp

--- codigos/expressoes_logicas.cpp
+++ codigos/expressoes_logicas.cpp
@@ -3,10 +3,14 @@ using namespace std;
  int main(){
      int n1=100;
      int n2=200;
-     bool expressao = (n1==100) && (n2==200);
+     bool expressao = (n1==100) && (n2==200); //&& (and)
      std::cout << "Resultado da expressao: " << expressao <<std::endl;
 
-     expressao = (n1==100)||(n2==100);
+     expressao = (n1==100)||(n2==100); // || (or)
+     std::cout << "Resultado da expressao: " << expressao <<std::endl;
+
+     expressao = !((n1==100)||(n2==100)); // ! (not)
      std::cout << "Resultado da expressao: " << expressao <<std::endl;
      return 0;
+
  }
