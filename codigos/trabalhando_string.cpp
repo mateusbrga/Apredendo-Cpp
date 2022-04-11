34cce5d83aae253da30c29d1ef1360e2d8f1da00
Mateus Braga <mateuscoclans@gmail.com>
2022-04-11 18:14:42 -0300

    Update trabalhando_string.cpp

--- codigos/trabalhando_string.cpp
+++ codigos/trabalhando_string.cpp
@@ -8,5 +8,14 @@ int main(){
     //[i] acessa a posicao do caracter e + contatena string
     string nome= "Mateus";
     std::cout << "Nome: " << nome << std::endl;
+    int tam;
+    tam= nome.size();
+    bool vazia;
+    vazia=nome.empty();
+    std::cout << "Tamanho dda string: " << tam << std::endl;
+    std::cout << "Está vazia?(0/1): " << vazia << std::endl;
+    /* outras funções: at(int i), erase(),  
+    replace(inicio, N de caracteres, string), find(), substr(), compare() */
+     
     }
     
