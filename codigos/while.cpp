8e4daa6a7594ed5985b9d795235e8b4987009c1d
Mateus Braga <mateuscoclans@gmail.com>
2022-04-13 17:41:20 -0300

    Create while.cpp

---
+++ codigos/while.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int i;
+    while(i!=0){
+        std::cout << "digite 0 para sair;" << std::endl;
+        std::cout <<"Digite um numero: ";
+        std::cin >> i;
+        std::cout <<"Numero digitado: " << i <<std::endl;;
+
+    }
+    return 0;
+}
