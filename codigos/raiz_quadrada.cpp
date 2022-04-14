3f5ecc8d3c97eb8ef4f73906a1f9a721c35a772e
Mateus Braga <mateuscoclans@gmail.com>
2022-04-13 23:22:13 -0300

    Create raiz_quadrada.cpp

---
+++ codigos/raiz_quadrada.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main(){
+    double raiz;
+    int n;
+    std::cout << "Calculadora de raiz quadrada!" <<std::endl; 
+    std::cout << "Digite um numero: ";
+    std::cin >> n;
+    raiz=sqrt(n);
+    std::cout << "Raiz: " << raiz <<std::endl; 
+}
